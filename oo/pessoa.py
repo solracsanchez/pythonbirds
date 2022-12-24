4693e34bba87287aec195db7428ecdf277dcd261
carlossanchez <olracsanchez@gmail.com>
2022-12-24 01:31:23 -0500

    Adicionada pergunta de numero de filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,24 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return 'Olá'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+
+    for v in range(int(input(f'Digite o numero filhos:'))):
+        filho = Pessoa()
+        filho.nome = input(f'Digite o nome do filho {v+1}: ')
+        filho.idade = int(input(f'Digite a idade do filho {v+1}: '))
+    pai = Pessoa(filho, nome='Mariano')
+    print(pai.cumprimentar())
+    print(pai.nome)
+    print(pai.idade)
+    for filho in pai.filhos:
+        print(filho.nome)
+        print(filho.idade)
+
